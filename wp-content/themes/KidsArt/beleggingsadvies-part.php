e7e944ef22c76567fd043f6de974c4ad74b5a0cb
jacobpetrovic85 <jacobpetrovic85@gmail.com>
2018-10-15 18:28:43 +0200

    CSS

--- wp-content/themes/KidsArt/beleggingsadvies-part.php
+++ wp-content/themes/KidsArt/beleggingsadvies-part.php
@@ -23,7 +23,7 @@
 
   <?php echo do_shortcode("[metaslider id=2095]"); ?>
 
-  <div>
+  <div class="wysiwyg">
       <?php
       if ( defined( 'ICL_LANGUAGE_CODE' ) ) {
           $ID = (ICL_LANGUAGE_CODE == 'nl') ? 1092 : 1;
@@ -31,39 +31,6 @@
       }
       ?>
   </div>
-  <div class="container">
-      <div class="row">
-          <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-              <div class="wrapper tekstlinks">
-                  <?php
-                  if ( defined( 'ICL_LANGUAGE_CODE' ) ) {
-                      $ID = (ICL_LANGUAGE_CODE == 'nl') ? 1092 : 1;
-                      printf(get_field('beleggingsadvies-header-tekst', $ID));
-                  }
-                  ?>
-              </div>
-          </div>
-          <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-              <div class="wrapper">
-                  <img src="<?php
-                            if ( defined( 'ICL_LANGUAGE_CODE' ) ) {
-                                $ID = (ICL_LANGUAGE_CODE == 'nl') ? 1092 : 1;
-                                printf(get_field('beleggingsadvies-foto', $ID));
-                            }
-                            ?>" class="asset-management-foto" />
-              </div>
-          </div>
-      </div>
-  </div>
-  <div>
-      <?php
-      if ( defined( 'ICL_LANGUAGE_CODE' ) ) {
-          $ID = (ICL_LANGUAGE_CODE == 'nl') ? 1092 : 1;
-          printf(get_field('beleggingsadvies-text3', $ID));
-      }
-      ?>
-  </div>
-
   <a href="<?php printf(get_site_url()) ?>/beleggingsadvies_/">
       <button>
           <?php
--- wp-content/themes/KidsArt/herontwikkel-part.php
+++ wp-content/themes/KidsArt/herontwikkel-part.php
@@ -23,44 +23,11 @@
 
   <?php echo do_shortcode("[metaslider id=2083]"); ?>
 
-  <div>
+  <div class="wysiwyg">
       <?php
       if ( defined( 'ICL_LANGUAGE_CODE' ) ) {
           $ID = (ICL_LANGUAGE_CODE == 'nl') ? 911 : 1;
           printf(get_field('herontwikkel-text2', $ID));
-      }
-      ?>
-  </div>
-  <div class="container">
-      <div class="row">
-          <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-              <div class="wrapper">
-
-                  <img src="<?php
-                            if ( defined( 'ICL_LANGUAGE_CODE' ) ) {
-                                $ID = (ICL_LANGUAGE_CODE == 'nl') ? 911 : 1;
-                                printf(get_field('herontwikkel-foto', $ID));
-                            }
-                            ?>" class="asset-management-foto" />
-              </div>
-          </div>
-          <div class="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-              <div class="wrapper tekstrechts">
-                  <?php
-                  if ( defined( 'ICL_LANGUAGE_CODE' ) ) {
-                      $ID = (ICL_LANGUAGE_CODE == 'nl') ? 911 : 1;
-                      printf(get_field('herontwikkel-header-tekst', $ID));
-                  }
-                  ?>
-              </div>
-          </div>
-      </div>
-  </div>
-  <div>
-      <?php
-      if ( defined( 'ICL_LANGUAGE_CODE' ) ) {
-          $ID = (ICL_LANGUAGE_CODE == 'nl') ? 911 : 1;
-          printf(get_field('herontwikkel-text3', $ID));
       }
       ?>
   </div>
